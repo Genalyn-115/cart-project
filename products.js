25d2e1b9058adf60708283bcb83a040e34c83db1
Genalyn Tobias Nerlund <77393454+Genalyn-115@users.noreply.github.com>
2021-04-04 21:52:16 +0200

    Initial commit

---
+++ products.js
@@ -0,0 +1,33 @@
+// BOOK PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Offermakaren",
+    desc : "Viveca Sten.",
+    img : "Offermakaren.jpeg",
+    price : 69
+  },
+  124: {
+    name : "The Secret",
+    desc : "Rhouda Bryne.",
+    img : "The Secret.jpeg",
+    price : 99
+  },
+  125: {
+    name : "The Flame",
+    desc : "Leonard Cohen.",
+    img : "The Flame.jpeg",
+    price : 55
+  },
+  126: {
+    name : "Selfies",
+    desc : "Adler Olsen. ",
+    img : "Selfies.jpeg",
+    price : 89
+  },
+  127: {
+    name : "Ödesmark",
+    desc : "Stina Jackson. ",
+    img : "Ödesmark.jpeg",
+    price : 119
+  }
+};
